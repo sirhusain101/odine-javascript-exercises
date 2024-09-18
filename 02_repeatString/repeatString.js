227f1a3e9b55d76d3c2f2b73933ab4392cffaea0
sirhusain101 <21244345+sirhusain101@users.noreply.github.com>
2024-09-18 17:22:47 +0530

    02_repeatString - done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let i = 0;
+  if (num < 0) {
+    string = "ERROR";
+  } else {
+    while (i <= num) {
+      return string.repeat(num);
+    }
+  }
+  return string;
 };
+
+console.log(repeatString(" ", 10));
 
 // Do not edit below this line
 module.exports = repeatString;
