4f9cbc899a203a2dd71be8667a4373afcd8f9958
sirhusain101 <21244345+sirhusain101@users.noreply.github.com>
2024-09-18 19:13:56 +0530

    03_reverseString - done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let strToArray = []; // empty array
 
+  for (let i = 0; i < str.length; i++) {
+    strToArray.push(str.at(i)); // converts each letter of the string into an array
+  }
+
+  strToArray.reverse(); // reverses letters in array
+  return strToArray.join(""); // converts array into string
 };
+
+console.log(reverseString(" "));
 
 // Do not edit below this line
 module.exports = reverseString;
