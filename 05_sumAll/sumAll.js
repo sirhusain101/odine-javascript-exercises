23bbf53eac323b7caa7b02426852348ff774809b
sirhusain101 <21244345+sirhusain101@users.noreply.github.com>
2024-09-18 21:54:13 +0530

    05_sumAll - done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
 
+  if (
+    a < 0 ||
+    b < 0 ||
+    !Number.isInteger(a) ||
+    !Number.isInteger(b) ||
+    typeof a === "string" ||
+    typeof b === "string"
+  ) {
+    return "ERROR";
+  } else if (a > b) {
+    [a, b] = [b, a];
+  }
+
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 };
+
+console.log(sumAll(10, [90, 1]));
 
 // Do not edit below this line
 module.exports = sumAll;
