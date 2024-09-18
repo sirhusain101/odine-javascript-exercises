9f9968aee62b42d75b973f3ddddcd3b8f1c4abeb
sirhusain101 <21244345+sirhusain101@users.noreply.github.com>
2024-09-18 20:58:12 +0530

    04_removeFromArray - done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...items) {
+  return arr.filter(function (i) {
+    return !items.includes(i);
+  });
 };
+
+console.log(removeFromArray([1, 2, 3], "1", 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
