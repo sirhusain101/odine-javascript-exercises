e998fe3781a809960e693253b212f2ebfa4b7f45
sirhusain101 <21244345+sirhusain101@users.noreply.github.com>
2024-09-19 15:22:01 +0530

    06_leapYears - done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false; // Not divisible by 4, it's not a leap year
+  }
 };
+
+console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
