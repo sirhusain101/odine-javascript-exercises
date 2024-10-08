b94975de24f103b442f96436d572dd94b5d41505
sirhusain101 <21244345+sirhusain101@users.noreply.github.com>
2024-10-08 15:36:33 +0530

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  alphaNumericStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  reverseStr = alphaNumericStr.split("").reverse().join("");
 
+  return reverseStr === alphaNumericStr ? true : false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
