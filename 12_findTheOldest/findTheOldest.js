1678d151770dfb36050a87a61b1eb5e7af257d01
sirhusain101 <21244345+sirhusain101@users.noreply.github.com>
2024-10-08 18:43:42 +0530

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const currentYear = new Date().getFullYear();
 
+  return array
+    .sort((a, b) => {
+      if (a.yearOfDeath === undefined || b.yearOfDeath === undefined) {
+        return currentYear - b.yearOfBirth - (currentYear - a.yearOfBirth);
+      } else {
+        return b.yearOfDeath - b.yearOfBirth - (a.yearOfDeath - a.yearOfBirth);
+      }
+    })
+    .filter((person, index) => index === 0)[0];
 };
 
 // Do not edit below this line
