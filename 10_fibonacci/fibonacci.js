96409c7dfc2227ea6504d7a0986c03ee7a2d0f2d
sirhusain101 <21244345+sirhusain101@users.noreply.github.com>
2024-10-08 18:01:42 +0530

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = parseInt(num);
 
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else if (num < 0) {
+    return "OOPS";
+  }
+
+  let a = 0;
+  let b = 1;
+  let member;
+
+  for (let i = 2; i <= num; i++) {
+    member = a + b;
+    a = b;
+    b = member;
+  }
+  return member;
 };
 
 // Do not edit below this line
